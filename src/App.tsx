1cd5efa934a4466d4c9ed32b45ea1f5b1fe6d8e2
Mayowa Yakub <mayoa.net@gmail.com>
2025-05-31 15:19:21 +0100

    Create the basic page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+function Header() {
+    return (
+        <header className="p-4 flex justify-between items-center">
+            <div className="flex items-center">Left Menu</div>
+            <div className="flex items-center flex-col">
+                <h1 className="text-2xl font-semibold flex">COACHING</h1>
+                <div className="text-sm text-gray-500">by Kimberley</div>
+            </div>
+            <div className="flex items-center">Right Menu</div>
+        </header>
+    );
+}
+
+function Main() {
+    return <main className="bg-gray-100 p-4 flex-1">Main Content</main>;
+}
+
+function Footer() {
+    return <footer className="p-4">Footer</footer>;
+}
+
 function App() {
-    return <h1 className="text-3xl font-bold">Coaching</h1>;
+    return (
+        <div className="flex flex-col h-screen">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
