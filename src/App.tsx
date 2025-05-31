1d19f8f65878cbe9127ff7457265b00019385cc7
Mayowa Yakub <mayoa.net@gmail.com>
2025-05-31 16:21:47 +0100

    Add custom fonts and update app styling with new header and jumbotron components

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,40 @@ function Header() {
         <header className="p-4 flex justify-between items-center">
             <div className="flex items-center">Left Menu</div>
             <div className="flex items-center flex-col">
-                <h1 className="text-2xl font-semibold flex">COACHING</h1>
-                <div className="text-sm text-gray-9">by Kimberley</div>
+                <h1 className="text-2xl font-semibold flex font-condensed">
+                    COACHING
+                </h1>
+                <div className="text-sm text-gray-9 font-cursive">
+                    by Kimberley
+                </div>
             </div>
             <div className="flex items-center">Right Menu</div>
         </header>
     );
 }
 
+// ----- Main Content -----
+
+function Jumbotron() {
+    return (
+        <div className="bg-gray-4 p-4 flex-1">
+            <h1 className="font-semibold flex justify-end items-center font-condensed">
+                <span className="text-2xl text-gray-12 font-condensed">
+                    WORK WITH
+                </span>
+                &nbsp; &nbsp;
+                <span className="text-2xl text-gray-12 font-cursive">Me</span>
+            </h1>
+        </div>
+    );
+}
+
 function Main() {
-    return <main className="bg-gray-4 p-4 flex-1">Main Content</main>;
+    return (
+        <main className="bg-gray-4 p-4 flex-1">
+            <Jumbotron />
+        </main>
+    );
 }
 
 function Footer() {
